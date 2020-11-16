3a7056aa7779c98a5af9614f15ecfe0f84492d68
yukendev <yuken0906dev@gmail.com>
2020-11-16 16:08:21 +0900

    Initial Commit

---
+++ IdeaStorage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IdeaStorage
+//
+//  Created by 手塚友健 on 2020/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
