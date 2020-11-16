d132ee9c7abd47fe5f9aa69d1356814c0b5c32e1
yukendev <yuken0906dev@gmail.com>
2020-11-16 22:19:37 +0900

    first commit

---
+++ IdeaStorage/View/CustomCell1.swift
@@ -0,0 +1,23 @@
+//
+//  CustomCell1.swift
+//  IdeaStorage
+//
+//  Created by 手塚友健 on 2020/11/16.
+//
+
+import UIKit
+
+class CustomCell1: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
